4c76ccdd60fc06033a9377406f0236436db0ec09
smilesMadhavi <72471453+smilesMadhavi@users.noreply.github.com>
2020-10-21 10:15:37 -0500

    Add files via upload

---
+++ AddSub.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+public class AddSub
+{
+	public static double check( int a, int b )
+	{
+		return 0;
+	}
+}
